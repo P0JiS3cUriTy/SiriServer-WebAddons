6facd7dc37b4fd69c2666a6bc0817c3c373ef29d
Cedric Boverie <ced@boverie.eu>
2012-03-03 19:24:20 +0100

    Added facebook to config file

--- config-sample.php
+++ config-sample.php
@@ -6,3 +6,7 @@ define("SERVER_PORT", "443");
 define("TWITTER_CONSUMER_KEY", "");
 define("TWITTER_CONSUMER_SECRET", "");
 define("TWITTER_CALLBACK", "http://192.168.1.101/SiriServer/twitter.php");
+
+define("FACEBOOK_APPID", "");
+define("FACEBOOK_APPSECRET","");
+define("FACEBOOK_CALLBACK", "http://192.168.1.101/SiriServer/facebook.php");
